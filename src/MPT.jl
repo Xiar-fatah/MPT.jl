268695ee2b21fba8f09023c4c0157a5b092e5953
Xiar-fatah <xiar.fatah@gmail.com>
2022-03-16 17:19:41 +0100

    Added needed packages

--- src/MPT.jl
+++ src/MPT.jl
@@ -1,5 +1,9 @@
 module MPT
 
 # Write your package code here.
+# Mabye we should not have to build in plots?
+# The user perhaps can plot themselves.
+using Statistics, XLSX, DataFrames, JuMP, Dates
+import Ipopt
 
 end
