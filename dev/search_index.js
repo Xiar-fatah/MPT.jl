891047a666c35a2b413d8738cb4c2acb8fe705ac
Documenter.jl <documenter@juliadocs.github.io>
2022-03-16 16:11:34 +0000

    build based on e9ae3e5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MPT","category":"page"},{"location":"#MPT","page":"Home","title":"MPT","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MPT.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MPT]","category":"page"}]
+}
