9453ae38cb22b6ad9a3c016c2376a3585ea2b8a2
Xiar-fatah <xiar.fatah@gmail.com>
2022-03-26 16:57:23 +0100

    Testing documenter.jl

--- src/portfolios.jl
+++ src/portfolios.jl
@@ -1,3 +1,23 @@
+"""
+    Arcsine(a,b)
+The *Arcsine distribution* has probability density function
+```math
+f(x) = \\frac{1}{\\pi \\sqrt{(x - a) (b - x)}}, \\quad x \\in [a, b]
+```
+```julia
+Arcsine()        # Arcsine distribution with support [0, 1]
+Arcsine(b)       # Arcsine distribution with support [0, b]
+Arcsine(a, b)    # Arcsine distribution with support [a, b]
+params(d)        # Get the parameters, i.e. (a, b)
+minimum(d)       # Get the lower bound, i.e. a
+maximum(d)       # Get the upper bound, i.e. b
+location(d)      # Get the left bound, i.e. a
+scale(d)         # Get the span of the support, i.e. b - a
+```
+External links
+* [Arcsine distribution on Wikipedia](http://en.wikipedia.org/wiki/Arcsine_distribution)
+Use `Arcsine(a, b, check_args=false)` to bypass argument checks.
+"""
 function equal_weights(returns::AbstractMatrix)::AbstractVector
     """
     Input: Returns over a specific period.
